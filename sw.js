56cd013f68d949af3fe1849d189c99275d40e4a0
yang739002895-debug <yang739002895@gmail.com>
2026-02-24 19:58:31 +0800

    123
    213

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'wedding-pwa-v1';
+const urlsToCache = [
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});

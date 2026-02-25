8c959164f4b60b759f51f7e2a2cfb319d1f0a560
yang739002895-debug <yang739002895@gmail.com>
2026-02-25 14:24:58 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,20 @@
-const CACHE_NAME = 'wedding-pwa-v1';
+const CACHE_NAME = 'wedding-pwa-v3'; // 版本号更新，强制刷新缓存
 const urlsToCache = [
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './back.png',             // 缓存背景图
+  './FZXiaoBiaoSong-B05S.woff2' // 缓存字体
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
       .then(() => self.skipWaiting())
   );
 });
@@ -17,7 +22,12 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });
 

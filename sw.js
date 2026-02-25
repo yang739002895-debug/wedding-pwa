c17d477778f0f17625b92c9a22257defd50fd2c3
yang739002895-debug <yang739002895@gmail.com>
2026-02-25 16:22:34 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wedding-pwa-v1.01'; // 版本号更新，强制刷新缓存
+const CACHE_NAME = 'wedding-pwa-v1.1'; // 版本号更新，强制刷新缓存
 const urlsToCache = [
   './index.html',
   './manifest.json',
@@ -35,5 +35,6 @@ self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
 
 });
+
 
 
